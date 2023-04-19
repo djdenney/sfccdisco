bfee1af78b870d315699add3c34ee85e699c1f65
djdenney <daniel@denneythe.dev>
2023-04-19 06:45:33 -0700

    changed to  format for http api

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ require("dotenv").config()
 const { ocapi } = require("./connections/ocapi")
 const { sfccam } = require("./connections/sfccam")
 
-const abTestSearch = async (siteId, searchData = {id: "", description: "", enabled: "", paused: "", key_metric_id: "", email_addresses: "", tags: "", expiration_type: "", start_date: "", end_date: ""}) => {
-    const type = 'POST'
-    const path = `sites/${siteId}/ab_test_search`
-    const data = Object.values(searchData).reduce((a, b, i) => {
-        b && b !== "" && a.query.filtered_query.filter.bool_filter.filters.push({term_filter: {field: Object.keys(searchData)[i], operator: "is", values: [b]}})
-        return a
-    }, {
-        query: {
-            filtered_query: {
-                query: { match_all_query: {} },
-                filter: {
-                    bool_filter: {
-                        operator: "and",
-                        filters: []
+const abTests = {
+    abTestSearch: async (siteId, searchData = {id: "", description: "", enabled: "", paused: "", key_metric_id: "", email_addresses: "", tags: "", expiration_type: "", start_date: "", end_date: ""}) => {
+        const type = 'POST'
+        const path = `/s/-/dw/data/v19_5/sites/${siteId}/ab_test_search`
+        const data = Object.values(searchData).reduce((a, b, i) => {
+            b && b !== "" && a.query.filtered_query.filter.bool_filter.filters.push({term_filter: {field: Object.keys(searchData)[i], operator: "is", values: [b]}})
+            return a
+        }, {
+            query: {
+                filtered_query: {
+                    query: { match_all_query: {} },
+                    filter: {
+                        bool_filter: {
+                            operator: "and",
+                            filters: []
+                        }
                     }
                 }
-            }
-         }
-    })
-    const response = await ocapi(type, path, data)
-    console.log(response)
+             }
+        })
+        const response = await ocapi(type, path, data)
+        return response
+    }
+}
+
+
+module.exports = {
+    abTests
 }
